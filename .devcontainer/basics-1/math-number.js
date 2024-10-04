0609aba3b426d3c61c0bf298788b3cde096494d3
Pranjal Kumar Srivastav <72075185+pranjalCode@users.noreply.github.com>
2024-10-04 06:44:08 +0000

    Math functions and operation on number

---
+++ .devcontainer/basics-1/math-number.js
@@ -0,0 +1,17 @@
+let n = 27
+let n1 = new Number(n) //it provide multiple functions object type
+//Both of the above are number but they provide differnt operation
+
+console.log(n)
+console.log(n1)
+
+//Math is built in library in js which perform multiple functions
+
+let s = Math.random()
+console.log(s) //gives the number between 0 - 1
+console.log(Math.floor(Math.random() *10))  //gives number between 1-10
+console.log(Math.ceil(26.8)) //gives the above number
+console.log(Math.floor(27.8))  //gives integer number ignoring decimal
+console.log(Math.pow(7,2)) //gives the exponantial numer
+console.log(Math.E)
+console.log(Math.trunc(27.2457))  //return only integer part
