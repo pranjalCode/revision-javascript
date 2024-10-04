ba5d5acda9b76838740aa582eaccdfb1a0896c1f
Pranjal Kumar Srivastav <72075185+pranjalCode@users.noreply.github.com>
2024-10-04 05:32:17 +0000

    DataType Conversion concept

---
+++ .devcontainer/basics-1/conversion-datatype.js
@@ -0,0 +1,20 @@
+let a = 27
+console.log(a)
+console.log(typeof a)
+//this is s number
+
+
+let numerInString = String(a)
+//This is String, it typecast number into String
+console.log(numerInString)
+console.log(typeof numerInString)
+
+// let nullValue = null(a)
+// console.log(typeof nullValue)
+// console.log(nullValue)
+//we can convert ant data type Using Number, String, Boolean
+
+console.log("27"<1)
+console.log(null == 1)
+console.log(undefined != 1)
+//these type of comparision not good for programming
