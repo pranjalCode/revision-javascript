0139cc27692b4b2ed170b5957d4880d1f0ff6fab
Pranjal Kumar Srivastav <72075185+pranjalCode@users.noreply.github.com>
2024-10-04 04:44:45 +0000

    variable and data types in javascript

---
+++ .devcontainer/basics-1/dataTypes-1.js
@@ -0,0 +1,24 @@
+"use strict";
+//This line treats all js code as new version
+
+//alert('any message)
+//not run in console, it runs on browser
+
+//There are 7 primitive datatype in javascript
+
+let lang = "javascript"
+//String " "
+
+let ver = 20.2
+//Number (Any integer, decimal, complex not alowed)
+
+let status1 = true
+//boolean (true/false) 
+
+let n = null
+//standalone value, it is value which have null value
+
+let d;
+//when we don't declare the value, it is undefined
+
+console.table([lang, status1, n. d]);   
---
+++ .devcontainer/basics-1/variables-1.js
@@ -0,0 +1,30 @@
+const nameId = 125475;
+//use const keyword to create variable when value does not change
+//Can't reassign value
+//Can't redeclare
+//functional scope
+
+
+
+let name1 = 'pranjal';
+//use let keyword when value will be change in future
+//Can reassign value
+//Can redclare
+//functional scope
+
+var email = 'pranshu@gmail.com'
+//Don/t use var in java it's old
+//can reassign, redclare, global scope
+
+city = 'ghazipur'
+//JavaScript provide easy convention to create variable without keyword
+//Don't use it
+
+let contact;
+//We can only create varible, don't need to assign value on creating
+
+console.log(nameId)
+//to print single variable, use console.log()
+
+console.table([nameId, name1, email, city, contact])
+//To print mltiple variable, use console.table([pass all the variable here])
